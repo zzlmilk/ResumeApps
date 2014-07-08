bce4a1cf30ad477fe95766b8fffd75ed5787ee02
xbruence <372260652@qq.com>
2014-07-08 10:46:30 +0800

    test file

---
+++ ResumeManage/ResumeManage/Views/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  ResumeManage
+//
+//  Created by 单好坤 on 14-7-8.
+//  Copyright (c) 2014年 单好坤. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
