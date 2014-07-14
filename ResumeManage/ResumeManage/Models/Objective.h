67be8d2ca694607077f9694f2148bbdc89b737f1
zzlmilk <413124766@qq.com>
2014-07-14 16:08:35 +0800

    objective add fiel

---
+++ ResumeManage/ResumeManage/Models/Objective.h
@@ -0,0 +1,14 @@
+//
+//  Objective.h
+//  ResumeManage
+//
+//  Created by zzlmilk on 14-7-14.
+//  Copyright (c) 2014年 单好坤. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Base.h"
+
+@interface Objective : Base
+
+@end
