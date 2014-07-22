ceb7801180048b838bc1202059415af942148b78
zzlmilk <413124766@qq.com>
2014-07-22 15:22:46 +0800

    dsd Please enter the commit message for your changes. Lines starting

---
+++ ResumeManage/ResumeManage/Views/MYBlurIntroductionView/SubclassPanel/FirstPanel.m
@@ -0,0 +1,37 @@
+//
+//  FirstPanel.m
+//  ResumeManage
+//
+//  Created by zzlmilk on 14-7-17.
+//  Copyright (c) 2014年 单好坤. All rights reserved.
+//
+
+#import "FirstPanel.h"
+
+@implementation FirstPanel
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
+
+
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
